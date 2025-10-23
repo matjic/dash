ecb06b3101c42f2b7064ce4238870c1b3f0dba9f
Mathew Jacob <5201181+matjic@users.noreply.github.com>
2025-10-22 20:30:22 -0500

    Initial Commit

---
+++ dashTests/dashTests.swift
@@ -0,0 +1,17 @@
+//
+//  dashTests.swift
+//  dashTests
+//
+//  Created by Mathew Jacob on 10/22/25.
+//
+
+import Testing
+@testable import dash
+
+struct dashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
